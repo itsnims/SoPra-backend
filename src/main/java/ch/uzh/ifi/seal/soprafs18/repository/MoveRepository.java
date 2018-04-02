64e24ff86223a9156f89f84f77d97bf83fe44153
naruchixx <nimra.ahmed.n0n2@gmail.com>
2018-04-02 16:34:36 +0200

    Deleted unnesssecary filrs from template

--- src/main/java/ch/uzh/ifi/seal/soprafs18/repository/MoveRepository.java
+++
@@ -1,8 +0,0 @@
-package ch.uzh.ifi.seal.soprafs18.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository("moveRepository")
-public interface MoveRepository extends CrudRepository<Move, Long> {
-}

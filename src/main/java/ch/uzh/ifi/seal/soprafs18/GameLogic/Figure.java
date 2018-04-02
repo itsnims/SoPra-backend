2f90ddd86a10c8a0eb968e76e19310cddff053f9
naruchixx <nimra.ahmed.n0n2@gmail.com>
2018-04-02 20:55:37 +0200

    initial push for the class playingpiece #13

---
+++ src/main/java/ch/uzh/ifi/seal/soprafs18/GameLogic/Figure.java
@@ -0,0 +1,23 @@
+package ch.uzh.ifi.seal.soprafs18.GameLogic;
+
+public class Figure {
+
+    int currentPosition;
+    String Color;
+
+    public void setCurrentPosition(int currentPosition) {
+        this.currentPosition = currentPosition;
+    }
+
+    public void setColor(String color) {
+        Color = color;
+    }
+
+    public int getCurrentPosition() {
+        return currentPosition;
+    }
+
+    /** am anfang farbe zuteilen
+     *
+     */
+}

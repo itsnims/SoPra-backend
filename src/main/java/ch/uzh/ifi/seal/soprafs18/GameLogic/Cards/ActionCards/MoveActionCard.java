963daa516e8066d13caeea950a90923a70713fde
naruchixx <nimra.ahmed.n0n2@gmail.com>
2018-04-02 20:48:52 +0200

    added final special function closes #1 and #27

--- src/main/java/ch/uzh/ifi/seal/soprafs18/GameLogic/Cards/ActionCards/MoveActionCard.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs18/GameLogic/Cards/ActionCards/MoveActionCard.java
@@ -5,12 +5,27 @@ import ch.uzh.ifi.seal.soprafs18.GameLogic.Player;
 
 
 public class MoveActionCard extends ActionCard {
+    Player player;
+    int position;
+
     public MoveActionCard(String name,String cardColour, Boolean reusable,Integer Price){
         super(name,cardColour,reusable,Price);
     }
+    public void setPlayer(Player player){
+        this.player = player;
+
+    }
+
+    public void positionChoice(int choice){
+        this.position=choice;
+    }
 
     @Override
-    public void doSpecialFunction() {;
+    public void doSpecialFunction() {
+        /**get new position over rest.
+         *
+         */
+        player.myFigure.setCurrentPosition(position);
 
     }
 }

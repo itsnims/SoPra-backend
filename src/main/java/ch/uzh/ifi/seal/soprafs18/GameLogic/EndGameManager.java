da5f545aa2b576131da0fc73df7a16110be814de
naruchixx <nimra.ahmed.n0n2@gmail.com>
2018-04-02 16:45:12 +0200

    Added the EndGameManager according #22

---
+++ src/main/java/ch/uzh/ifi/seal/soprafs18/GameLogic/EndGameManager.java
@@ -0,0 +1,22 @@
+package ch.uzh.ifi.seal.soprafs18.GameLogic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EndGameManager  {
+    List<Player> Winners = new ArrayList<>(4);
+
+    public EndGameManager(){
+
+    }
+
+    public void CheckifReached(){
+        /**
+         * if (ElDorado.reachers.size() > 0){
+         * Winners.addAll(ElDorado.reachers)
+         *
+         */
+
+
+    }
+}

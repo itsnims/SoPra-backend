598416ca90b5a448faeb437a2d12298b8cbcc8aa
Alexandra Markovic <alexandramarkovic@AlexandsMacBook.fritz.box>
2018-04-07 23:00:17 +0200

    added round() function #21

--- src/main/java/ch/uzh/ifi/seal/soprafs18/GameLogic/Game.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs18/GameLogic/Game.java
@@ -1,6 +1,7 @@
 package ch.uzh.ifi.seal.soprafs18.GameLogic;
 
 import ch.uzh.ifi.seal.soprafs18.GameLogic.Cards.Card;
+import ch.uzh.ifi.seal.soprafs18.GameLogic.BoardPart.ElDorado;
 
 
 import java.util.ArrayList;
@@ -10,20 +11,43 @@ public class Game {
     public List<Player> Players = new ArrayList<>(4);
     public List<Card> TrashedCards = new ArrayList<>(80);
     public Market GameMarket = new Market();
+    public ElDorado elDorado =new ElDorado();
+    public int roundNum=0;
+    public Player Winner;
+    public List<Player> Winners = new ArrayList<>(4);
+    private int i, j;
+    private Player k;
 
     public Game(){
 
     }
 
-    public void addPlayer(Player player){
-        Players.add(player);
-    }
+    public void addPlayer(Player player){ Players.add(player); }
 
     public void round(){
+        for(i=0;i<Players.size();i++){
+            /** Players[i].execute...? turn of player */
+        }
+        if(elDorado.isReached()){
+            if (Winners.size() == 1) {
+                Winner = Winners.get(0);
+            } else if (Winners.size() > 1) {
+                k= Winners.get(0);
+                for(j=0;j<Winners.size();j++){
+                    if(k.getBlockadePoints()< Winners.get(j).getBlockadePoints()){
+                        k= Winners.get(j);
+                    }
+                }
+            }
+        }
+        roundNum=roundNum+1;
 
     }
 
+    public int getRoundNum() { return roundNum; }
 
+    public void Win(Player player) { Winners.add(player); }
 
+    public int getNumFigures() { return Players.size(); }
 
 }

7db452de42b7f6cb291b0eff5466f4078a90fa87
naruchixx <nimra.ahmed.n0n2@gmail.com>
2018-04-08 12:27:20 +0200

    Closes #22"

--- src/main/java/ch/uzh/ifi/seal/soprafs18/GameLogic/EndGameManager.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs18/GameLogic/EndGameManager.java
@@ -1,22 +1,42 @@
 package ch.uzh.ifi.seal.soprafs18.GameLogic;
 
+import ch.uzh.ifi.seal.soprafs18.GameLogic.BoardPart.ElDorado;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class EndGameManager  {
-    List<Player> Winners = new ArrayList<>(4);
+    Player Winner;
+    ElDorado elDorado = new ElDorado();
+    private int i, j;
+    private Player k;
 
     public EndGameManager(){
 
     }
 
-    public void CheckifReached(){
-        /**
-         * if (ElDorado.reachers.size() > 0){
-         * Winners.addAll(ElDorado.reachers)
-         *
-         */
-
-
+    public boolean CheckifReached(){
+        if (elDorado.isReached() == true){
+            return true;
+        }
+        return false;
     }
+
+    public Player getWinner(){
+        if (elDorado.getReachers().size() == 1) {
+            Winner = elDorado.getReachers().get(0);
+        }
+        else if (elDorado.getReachers().size() > 1) {
+            k= elDorado.getReachers().get(0);
+            for(j=0;j<elDorado.getReachers().size();j++){
+                if(k.getBlockadePoints()< elDorado.getReachers().get(j).getBlockadePoints()){
+                    k= elDorado.getReachers().get(j);
+                }
+            }
+        }
+        return k;
+    }
+
+
+
 }

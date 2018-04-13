633043286a7c63d411bf91eed38e7ee73a488527
angelabobo <markovic.angela.97@icloud.com>
2018-04-13 20:15:41 +0200

    added PlayExpeditionCard test #12

---
+++ src/test/java/ch/uzh/ifi/seal/soprafs18/GameLogic/Turns/PlayExpeditionCardTest.java
@@ -0,0 +1,29 @@
+package ch.uzh.ifi.seal.soprafs18.GameLogic.Turns;
+
+import ch.uzh.ifi.seal.soprafs18.GameLogic.Cards.Card;
+import ch.uzh.ifi.seal.soprafs18.GameLogic.Cards.ExpeditionCard;
+import ch.uzh.ifi.seal.soprafs18.GameLogic.Player;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class PlayExpeditionCardTest {
+    ExpeditionCard Scout1 = new ExpeditionCard("Scout","Green",true,1,2);
+    Player currentplayer = new Player("Green");
+    List<ExpeditionCard> selectedCards = new ArrayList<>();
+
+    @Test
+    public void turnfunction() {
+        selectedCards.add(Scout1);
+        currentplayer.handcards.addAll(selectedCards);
+        PlayExpeditionCard playsth = new PlayExpeditionCard(selectedCards,currentplayer);
+        playsth.turnfunction();
+        Assert.assertEquals(1, currentplayer.selection.size());
+        Assert.assertEquals(0, currentplayer.handcards.size());
+
+    }
+}

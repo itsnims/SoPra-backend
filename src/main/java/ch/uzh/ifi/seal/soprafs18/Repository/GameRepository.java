44671bd351bbc393bef0014d053b2eeba3a06afd
angelabobo <markovic.angela.97@icloud.com>
2018-04-15 23:01:08 +0200

    modified

--- src/main/java/ch/uzh/ifi/seal/soprafs18/Repository/GameRepository.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs18/Repository/GameRepository.java
@@ -1,9 +1,9 @@
 package ch.uzh.ifi.seal.soprafs18.Repository;
 
-import ch.uzh.ifi.seal.soprafs18.GameLogic.Game;
-import org.springframework.data.repository.CrudRepository;
+        import ch.uzh.ifi.seal.soprafs18.GameLogic.Game;
+        import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
+        import java.util.List;
 
 public interface GameRepository extends CrudRepository<Game,Long> {
     Game findByName(String name);

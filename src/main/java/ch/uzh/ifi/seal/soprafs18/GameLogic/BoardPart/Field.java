0bbe275f333050cf057b7c553b384d51a71fc0ff
alexandraa97 <markovicalexandra9@gmail.com>
2018-04-17 18:46:33 +0200

    added getBlockadeFromNeighbours() method

--- src/main/java/ch/uzh/ifi/seal/soprafs18/GameLogic/BoardPart/Field.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs18/GameLogic/BoardPart/Field.java
@@ -1,12 +1,12 @@
 package ch.uzh.ifi.seal.soprafs18.GameLogic.BoardPart;
 
-import ch.uzh.ifi.seal.soprafs18.GameLogic.Board;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Transient;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
@@ -17,35 +17,31 @@ public class Field extends BoardPiece {
 
     @Transient
     @JsonIgnore
-    public boolean TrashCards;
-
-    @Transient
-    @JsonIgnore
     public List<BoardPiece> neighbours = new ArrayList<>();
 
     @Transient
     @JsonIgnore
-    public boolean Accessable; /** not taken */
+    public boolean Accessable;
 
-    public Field(int strenght, String color, boolean PlayAnyCard, boolean TrashCard, boolean Accessable) {
-        super(strenght, color, PlayAnyCard);
-        this.Accessable=Accessable;
-        this.TrashCards=TrashCard;
+    /**
+     * not taken
+     */
+
+    public Field(int strenght, String color, boolean Accessable, String name) {
+        super(strenght, color);
+        this.name = name;
+        this.Accessable = Accessable;
 
     }
 
-    public boolean getAccessable(){
+    public boolean getAccessable() {
         return Accessable;
     }
 
-    public boolean getTrashCards(){
-        return TrashCards;
-    }
-
-    public void AddNewNeighbour(BoardPiece neighbour, BoardPiece... boardPieces){
+    public void AddNewNeighbour(BoardPiece neighbour, BoardPiece... boardPieces) {
         neighbours.add(neighbour);
-        if (boardPieces.length > 0){
-            for(int i=0; i < boardPieces.length; i++){
+        if (boardPieces.length > 0) {
+            for (int i = 0; i < boardPieces.length; i++) {
                 neighbours.add(boardPieces[i]);
             }
         }
@@ -57,5 +53,14 @@ public class Field extends BoardPiece {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Blockade getBlockadeFromNeighbours() {
+        for (int i = 0; i < neighbours.size(); i++) {
+            if (neighbours.get(i) instanceof Blockade) {
+                return (Blockade) neighbours.get(i);
+            }
+        }
+        return null;
     }
 }

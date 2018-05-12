ffdb5ff7133890ee8eddbea315e57bab69d685d1
naruchixx <nimra.ahmed.n0n2@gmail.com>
2018-05-12 19:53:47 +0200

    Corrected so that it wouldnt return occupied fields

--- src/main/java/ch/uzh/ifi/seal/soprafs18/GameLogic/BoardPart/Field.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs18/GameLogic/BoardPart/Field.java
@@ -90,9 +90,9 @@ public class Field extends BoardPiece {
 
     @JsonIgnore
     private Boolean getUsable(String Color, int Strenght,Field compare) {
-        if (compare.getColor().equals(Color)) if (Strenght >= compare.getStrenght()) return true;
-        if (compare.getColor().equals("White")) return true;
-        if (compare.getColor().equals("Camp")) return true;
+        if (compare.getColor().equals(Color)) if (Strenght >= compare.getStrenght()) if (compare.getAccessable())return true;
+        if (compare.getColor().equals("White")) if (compare.getAccessable()) return true;
+        if (compare.getColor().equals("Camp")) if (compare.getAccessable()) return true;
         return false;
     }
 
